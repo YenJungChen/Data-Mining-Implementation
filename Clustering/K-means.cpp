97e07668e52f1bc5ad51db583acebf8ccae88f05
Iris Chen <candyfairest@hotmail.com>
2017-05-02 03:09:02 -0500

    training/record

--- Clustering/K-means.cpp
+++ Clustering/K-means.cpp
@@ -2,111 +2,143 @@
 #include <deque>
 #include <math.h>
 #include <time.h>
+#include <string.h>
+#include <fstream>
 //define the longest distance between center and data point
 #define MAX 9999
-
+#define SIZE 514
+#define ATTR 8
 //the amount of clusters
-#define k 4
+#define k 8
 using namespace std;
 
-//define data structure
-struct Data {
-    int x;
-    int y;
+class DATA{
+public:
+    double data[8];
     int index;
-    int distance;
-};
+    double distance;
+}D[SIZE];
+double w[8]={0.0084468, 0.72183, 0.0752224, 0.0623064, 0.00354434, 0.0161686, 0.0395071, 0.0729745};
+
+void readin(){
+    ifstream fin;
+    char output[100];
+    int a=0, b=0;
+    char *pch;
+    fin.open("training.txt");
+    if(fin.is_open()){
+        while(!fin.eof()){
+            fin.getline(output,sizeof(output),'\n');
+            pch=strtok (output,",");
+            b=0;
+            while(pch!=NULL){
+                if(b!=ATTR)
+                    D[a].data[b]=w[b]*atof(pch);
+                //printf("%f\n",D[a].data[b++]);
+                pch=strtok (NULL, ",");
+                D[a].index=a;
+                b++;
+            }
+            a++;
+        }
+    }
+    fin.close();
+}
 
 //use the average X and Y of the cluster to find a closest new center
-int NewCenter(int AvgX, int AvgY, deque <Data> Cluster) {
-    int size = (int)Cluster.size();
-    int diff = 9999;
-    int index = -1;
-    for (int i=0; i<size; i++) {
+int NewCenter(double Avg[ATTR], deque <DATA> Cluster) {
+    double diff=9999;
+    int index=-1;
+    for (int i=0; i<Cluster.size(); i++) {
         //find the closest real point with the computational center point
-        int current_diff = (int)sqrt((AvgX - Cluster[i].x)*(AvgX - Cluster[i].x)+(AvgY - Cluster[i].y)*(AvgY - Cluster[i].y));
-        if(current_diff < diff) {
-            diff = current_diff;
-            index = i;
+        double current_diff=0;
+        for(int j=0; j<ATTR; j++)
+            current_diff+=pow(Avg[j]-Cluster[i].data[j], 2);
+        current_diff=sqrt(current_diff);
+        if(current_diff<diff) {
+            diff=current_diff;
+            index=i;
         }
     }
     return index;
 }
 
 //compute the average X and Y in the cluster
-Data Mean(deque <Data> Cluster) {
-    int SumX = 0;
-    int SumY = 0;
-    int Size = (int)Cluster.size();
-    for (int i=0; i<Size; i++) {
-        SumX += Cluster[i].x;
-        SumY += Cluster[i].y;
+DATA Mean(deque <DATA> Cluster) {
+    double SUM[ATTR];
+    for(int i=0; i<ATTR; i++)
+        SUM[i]=0;
+    for (int i=0; i<Cluster.size(); i++) {
+        for(int j=0; j<ATTR; j++)
+            SUM[j]+=Cluster[i].data[j];
     }
-    int avgX = SumX/Size;
-    int avgY = SumY/Size;
-    int index = NewCenter(avgX, avgY, Cluster);
-    return Cluster[index];
+    double AVG[8];
+    for(int i=0; i<8; i++){
+        AVG[i]=SUM[i]/SIZE;
+    }
+    int INDEX=NewCenter(AVG, Cluster);
+    return Cluster[INDEX];
 }
 
 //print the result of the cluster
-void PrintCluster(deque <Data> Cluster) {
-    int size = (int)Cluster.size();
-    for(int i=0; i<size; i++) {
-        cout<<"index: "<<Cluster[i].index;
-        cout<<", X: "<<Cluster[i].x;
-        cout<<", Y: "<<Cluster[i].y<<endl;
+void PrintCluster(deque <DATA> Cluster) {
+    cout<<"index: ";
+    for(int i=0; i<Cluster.size(); i++) {
+        cout<<Cluster[i].index<<", ";
     }
     cout<<endl;
 }
 
 //print the center of the cluster
-void PrintCenter(Data Center) {
-    cout<<"center: "<<Center.index;
-    cout<<", X: "<<Center.x;
-    cout<<", Y: "<<Center.y<<endl;
-    cout<<endl;
+void PrintCenter(DATA Center) {
+    cout<<"center: "<<Center.index<<endl<<endl;
 }
 
-void SumDist(deque <Data> d) {
-    int sum = 0;
+void SumDist(deque <DATA> d) {
+    int sum=0;
     for(int i=0; i<d.size(); i++)
-        sum += d[i].distance;
+        sum+=d[i].distance;
     cout<<"sum: "<<sum<<endl;
     cout<<endl;
 }
 
 int main(void) {
+    readin();
     srand((unsigned)time(NULL));
-    int n = 20;								//number of data
-    
-    //n data points (pre-set)
-    Data data[20] = {{1,3,0}, {3,5,1}, {2,4,2}, {16,20,3},
-        {14,18,4}, {22,1,5}, {20,3,6}, {19,2,7},
-        {18,17,8}, {7,5,9}, {24,6,10}, {8,8,11},
-        {20,18,12}, {5,3,13}, {4,4,14}, {15,15,15},
-        {18,5,16}, {6,8,17}, {13,23,18}, {23,1,19}};
-    
-    Data temp_cent[k];						//random centers
-    for (int a=0; a<k; a++) 				//the first time decide the centers randomly
-        temp_cent[a] = data[rand()%n];		//randomly choose the centers
+    DATA temp_cent[k];						//random centers
+    for (int i=0; i<k; i++){ 				//the first time decide the centers randomly
+        temp_cent[i] = D[rand()%SIZE];         //randomly choose the centers
+        for (int j=0; j<i; j++){
+            if (temp_cent[i].index==temp_cent[j].index) {
+                i--;
+                break;
+            }
+        }
+        
+    }
+
     bool stop = 0;
     while (!stop) {
-        deque <Data> cluster[k];			//store the divided data points in clusters
-        for (int i=0; i<n; i++) {			//divide data points into clusters
-            int last_dist = MAX;			//the distance between data points and each centers
+        deque <DATA> cluster[k];			//store the divided data points in clusters
+        for (int i=0; i<SIZE; i++) {		//divide data points into clusters
+            double last_dist = MAX;			//the distance between data points and each centers
             int cent = -1;					//store which center is the nearest
             
             //find the nearest center of each data points
             for (int j=0; j<k; j++) {
-                int curr_dist = (int)sqrt((data[i].x-temp_cent[j].x)*(data[i].x-temp_cent[j].x)+(data[i].y-temp_cent[j].y)*(data[i].y-temp_cent[j].y));
-                if (curr_dist < last_dist) {
-                    last_dist = curr_dist;
-                    cent = j;
+                int curr_dist=0;
+                for(int a=0; a<ATTR; a++)
+                    curr_dist+=pow(temp_cent[j].data[a]-D[i].data[a], 2);
+                curr_dist=sqrt(curr_dist);
+                if (curr_dist<last_dist) {
+                    last_dist=curr_dist;
+                    cent=j;
                 }
             }
-            data[i].distance = last_dist;
+            D[i].distance = last_dist;
             //add data points into the nearest cluster
-            cluster[cent].push_back(data[i]);
+            cluster[cent].push_back(D[i]);
+ 
         }
         
         //count how many centers are unchange to decide whether already find out the real center
